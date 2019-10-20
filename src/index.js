5bef77d40afc134906292b92c1efc3e8949d7382
yamam00s <s.yamamo0327@gmail.com>
2019-10-20 23:33:29 +0900

    onloadで発火確認

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import getGeoLocation from './util/getGeoLocation';
+import getDisaster from './util/getDisaster';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const currentPosition = await getGeoLocation();
+  const disasterData = await getDisaster('桜島', {
+    startDate: new Date('2013-01-01'),
+    endDate: new Date('2013-02-01')
+  })
+  console.log(currentPosition);
+  console.log(disasterData);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/util/getDisaster.js
+++ src/util/getDisaster.js
@@ -4,9 +4,7 @@ export default async (locationStr, {startDate, endDate}) => {
   let result;
   try {
     result = await axios.get(
-      `http://api.aitc.jp/jmardb-api/search
-        ?datetime=${startDate}datetime=${endDate}
-        &areaname=${locationStr}`
+      `http://api.aitc.jp/jmardb-api/search?datetime=${startDate}&datetime=${endDate}&areaname=${locationStr}`
     ).res;
   } catch(error) {
     result = error.massage;
