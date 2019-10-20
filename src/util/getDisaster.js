2efa75e34d2a4a75b51f18b9f766582445a65e15
yamam00s <s.yamamo0327@gmail.com>
2019-10-20 21:32:15 +0900

    create getDisaster.js

---
+++ src/util/getDisaster.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default async (locationStr, {startDate, endDate}) => {
+  let result;
+  try {
+    result = await axios.get(
+      `http://api.aitc.jp/jmardb-api/search
+        ?datetime=${startDate}datetime=${endDate}
+        &title=${locationStr}`
+    ).res;
+  } catch(error) {
+    result = error.massage;
+  }
+  return result;
+}
