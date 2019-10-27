1bac98e5b9be86db04a756a949cb457494523086
yamam00s <s.yamamo0327@gmail.com>
2019-10-28 03:06:54 +0900

    careate Geocodingモジュール

---
+++ src/util/getGeocoding.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import apiKey from '../apiKey';
+
+export default async (address) => {
+  let result;
+  try {
+    result = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json`,
+      {
+        params: {
+          address: address,
+          key: apiKey
+        }
+      }
+    );
+  } catch(error) {
+    result = error.massage;
+  }
+  return result;
+}
